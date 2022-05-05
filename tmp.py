d670c195937501a14af72bb8752d724e7cf0bbf0
nevdullc <95062984+nevdullc@users.noreply.github.com>
2022-05-05 12:45:52 -0400

    Add files via upload

---
+++ tmp.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Comment for utility and administrative tasks."""
+
+# from * import *
+import timeit
+
+def def_timer():
+    pass
+
+# main <--
+def main():
+    print(" ok.. ")
+    print('def timer\t\t', timeit.timeit(def_timer, number=0))
+
+# name space main <--
+if __name__ == '__main__':
+    main()
+
+
+#nevdullc
